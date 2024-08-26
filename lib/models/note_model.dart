0af0aec982f43ea193c91865ade4003d6a67db7a
Your Name <Abdulrahman956>
2024-08-26 11:18:11 +0300

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+class NoteModel {
+  final String title;
+  final String desc;
+  final String date;
+  final int color;
+
+  NoteModel({
+    required this.title,
+    required this.desc,
+    required this.date,
+    required this.color,
+  });
+}
