a666657e7f0bebc48bda3bb27d387ab508914663
Your Name <Abdulrahman956>
2024-08-29 13:39:51 +0300

    create notes fetch method

--- lib/cubits/note_cubit/note_cubit.dart
+++ lib/cubits/note_cubit/note_cubit.dart
@@ -1,8 +1,18 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
 
+import '../../models/note_model.dart';
 import 'note_cubit_states.dart';
 
 class NoteCubit extends Cubit<NoteCubitStates>{
   NoteCubit() : super(NoteInitialState());
-
+fetchAllNotes(){
+  try{
+    var noteBox =  Hive.box<NoteModel>('note box');
+    var notes = noteBox.values.toList();
+    emit(NoteSuccessState(notes));
+  }catch(e){
+    emit(NoteFailureState(e.toString()));
+  }
+}
 }
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../cubits/note_cubit/note_cubit.dart';
 import '../widgets/custom_bottom_sheet.dart';
 import '../widgets/notes_view_body.dart';
 
@@ -8,26 +10,29 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16)),
-              context: context,
-              isScrollControlled: true,
-              builder: (context) {
-                return Padding(
-                  padding: EdgeInsets.only(
-                    bottom: MediaQuery.of(context).viewInsets.bottom,
-                  ),
-                  child: const CustomBottomSheet(),
-                );
-              });
-        },
-        child: const Icon(Icons.add),
+    return BlocProvider(
+      create: (context) => NoteCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16)),
+                context: context,
+                isScrollControlled: true,
+                builder: (context) {
+                  return Padding(
+                    padding: EdgeInsets.only(
+                      bottom: MediaQuery.of(context).viewInsets.bottom,
+                    ),
+                    child: const CustomBottomSheet(),
+                  );
+                });
+          },
+          child: const Icon(Icons.add),
+        ),
+        body: const SafeArea(child: NotesViewBody()),
       ),
-      body: const SafeArea(child: NotesViewBody()),
     );
   }
 }
